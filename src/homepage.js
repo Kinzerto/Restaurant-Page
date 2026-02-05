0546a3e0fe28975b2572ae7c0f9e0281c033aaef
Kinth Enrique Cabuello <blood4249@gmail.com>
2026-02-06 05:21:56 +0800

    update play button SVG color and remove commented-out favorite item code

--- src/homepage.js
+++ src/homepage.js
@@ -61,7 +61,7 @@ export function homepageLoad(parent) {
 
     const path = document.createElementNS(svgNS, "path");
     path.setAttribute("d", "M22.5 10.7987C24.5 11.9534 24.5 14.8401 22.5 15.9948L4.49999 26.3871C2.49999 27.5418 -1.34201e-06 26.0984 -1.24107e-06 23.789L-3.32543e-07 3.00442C-2.31596e-07 0.695016 2.5 -0.748354 4.5 0.406346L22.5 10.7987Z");
-    path.setAttribute("fill", "#1E1E1E");
+    path.setAttribute("fill", "#7f1515");
 
     svg.appendChild(path);
     playButton.appendChild(svg);
@@ -112,61 +112,4 @@ export function homepageLoad(parent) {
     }
     createFavorite(specialty, food1, "Spicy noodles", "23.00", 3);
     createFavorite(specialty, food2, "Spicy noodles", "23.00", 3);
-
-    // const fav1 = createElem('div', 'fav1', specialty);
-    // const fav1Menu = createElem('img', 'fav1-img', fav1)
-    // fav1Menu.src = food1;
-
-    // //Fav1 Container
-    // const fav1MenuDetails = createElem('div', 'fav1MenuDetails', fav1);
-    // // Its Content
-    // const foodName1 = createElem('div', 'foodName', fav1MenuDetails);
-    // foodName1.textContent = 'Spicy noodles';
-    // const ratings1 = createElem('div', 'ratings', fav1MenuDetails);
-    // // star rating
-    // for (let i = 1; i <= 5; i++) {
-    //     const star = createElem('img', 'star', ratings1);
-    //     if (i <= 3) {
-    //         star.src = starFilled;
-    //     } else {
-    //         star.src = starUnFilled;
-    //     }
-    // }
-    // const price1 = createElem('div', 'price', fav1MenuDetails);
-    // const dollar1 = createElem('span', 'dollarSign', price1);
-    // dollar1.textContent = '$';
-    // const amount1 = createElem('span', 'amount', price1);
-    // amount1.textContent = '23.00';
-
-    // const fav2 = createElem('div', 'fav2', specialty);
-    // // FAV2 CONTAINER
-    // const fav2Menu = createElem('img', 'fav2-img', fav2)
-    // fav2Menu.src = food2;
-
-    // //Fav2 Container
-    // const fav2MenuDetails = createElem('div', 'fav2MenuDetails', fav2);
-    // // Its Content
-    // const foodName2 = createElem('div', 'foodName', fav2MenuDetails);
-    // foodName2.textContent = 'Spicy noodles';
-    // const ratings2 = createElem('div', 'ratings', fav2MenuDetails);
-    // // star rating
-    // for (let i = 1; i <= 5; i++) {
-    //     const star = createElem('img', 'star', ratings2);
-    //     if (i <= 3) {
-    //         star.src = starFilled;
-    //     } else {
-    //         star.src = starUnFilled;
-    //     }
-    // }
-    // const price2 = createElem('div', 'price', fav2MenuDetails);
-    // const dollar2 = createElem('span', 'dollarSign', price2);
-    // dollar2.textContent = '$';
-    // const amount2 = createElem('span', 'amount', price2);
-    // amount2.textContent = '23.00';
-
-    // const width = window.innerWidth;
-    // const height = window.innerHeight;
-
-    // console.log(`Width: ${width}px, Height: ${height}px`);
-
 }
