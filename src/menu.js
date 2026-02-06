fe21de0ee08a4d310ccc543242078c5ee0ec6e45
Kinth Enrique Cabuello <blood4249@gmail.com>
2026-02-06 09:40:34 +0800

    refactor menuPage function to improve structure and readability

--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,10 @@
+import { createElem } from "./homepage.js";
+
 export function menuPage(){
+    const menuPage = createElem('div', 'menuPage', content);
     document.title = "Menu";
+
+    const costumerFav = createElem('div', 'customerFav', menuPage);
+
+    costumerFav.textContent = "Customer Favorites";
 }
